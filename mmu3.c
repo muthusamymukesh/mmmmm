09f972cd38d52cfdc5532c40bd423404c266fdca
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-01-30 16:38:09 +0530

    Create mmu3.c

---
+++ mmu3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[20];
+int i,n,r,sum=0;
+clrscr();
+printf("enter the size of array");
+scanf("%d",&n);
+printf("enter r values of sum");
+scanf("%d",&r);
+printf("enter elements of array",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a(i));
+}
+for(i=o;i<r;i++)
+{
+sum=sum+a(i);
+}
+printf("sum of r values are %d",sum);
+}
+getch();
+}
